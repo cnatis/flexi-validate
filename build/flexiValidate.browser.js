f554914e263780127c533eb5b4dee21ef85c5ef4
Christian Natis <natis.christian@gmail.com>
2016-02-25 14:38:31 +0000

    Update dev dependencies

--- build/flexiValidate.browser.js
+++ build/flexiValidate.browser.js
@@ -47,11 +47,12 @@ var flexiValidate =
 
 	'use strict';
 
-	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
+
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
 	exports.isValid = isValid;
 	exports.isValidAsync = isValidAsync;
 	exports.messages = messages;
